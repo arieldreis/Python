46d10906dacea0d8ad5fa7c1c710d49923051546
Ariel Marinho <arieldreis@gmail.com>
2024-11-22 12:01:04 -0300

    Atualização

---
+++ ex006.py
@@ -0,0 +1,7 @@
+number = int(input('Digite um número: '))
+dobro = number * 2
+triplo = number * 3
+raiz = number ** 0.5
+print('O dobro de {} vale {}'.format(number,dobro))
+print('O triplo de {} vale {}'.format(number,triplo))
+print('A raiza quadrada de {} vale {}'.format(number,raiz))
