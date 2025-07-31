eebc037bab72a4ec9a31762479078ea3f6194672
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-07-31 16:32:00 -0300

    Updating some files

--- mundo3/ex095.py
+++ mundo3/ex095.py
@@ -1,25 +1,27 @@
-lista = []
-dadosjogador = {}
-dadosjogador["nome_jogador"] = str(input("Nome do Jogador: "))
-dadosjogador["partidas"] = int(input(f"Quantas partidas {dadosjogador['nome_jogador']} jogou: "))
-for cont in range(0, dadosjogador['partidas']):
-    dadosjogador['gols'] = int(input(f"Quantos gols na {cont}ª partida: "))
-    lista.append(dadosjogador['gols'])
-dadosjogador['soma'] = sum(lista) # A soma da de gols
-dicionario = {
-    "nome": dadosjogador['nome_jogador'],
-    "gols": lista,
-    "total": dadosjogador['soma']
-}
-print("=-"*30)
-print(f"{dicionario}")
-print("=-"*30)
-print("=-"*30)
-print(f"O jogagor {dicionario['nome']} jogou {dadosjogador['partidas']} partidas")
-for indice, gol in enumerate(lista):
-    print(f" => Na patida {indice}, fez {gol} gols.")
-print("=-"*30)
-
+jogadoresArray = []
+jogadoresDicio = {}
+somaGols = []
+while True:
+    jogadoresDicio["jogador"] = str(input("Nome do jogador: "))
+    jogadoresDicio["numeroPartidas"] = int(input(f"Quantas partidas {jogadoresDicio['jogador']} jogou: "))
+    for cont in range(0, jogadoresDicio['numeroPartidas']):
+        jogadoresDicio['numeroGols'] = int(input(f"Quantos gols na partida {cont}: "))
+    confirmacao = str(input("Quer Continuar [S/N]: ")).capitalize().lower()
+    while confirmacao != "S" and confirmacao != "N":
+        print("ERRO! Responda S ou N.")
+        confirmacao = str(input("Quer Continuar [Sim/Não]: ")).capitalize().lower()
+    if confirmacao == "N":
+        print("Todos os usuários foram cadastrados com sucesso.")
+        break
+    if confirmacao == "S":
+        print("Usuário cadastrado com sucesso!")
+    somaGols.append(jogadoresDicio['numeroGols'])
+    players = {
+        "nomeJogador": jogadoresDicio['jogador'],
+        "gols": jogadoresDicio['numeroGols'],
+        "total": sum(somaGols)
+    }
+print(players)
 '''Permite iterar sobre sequências enquanto acompanhamos o 
 índice de cada elemento. Ela facilita a manipulação dos dados durante a iteração 
 e é especialmente útil quando precisamos percorrer várias sequências simultaneamente'''
