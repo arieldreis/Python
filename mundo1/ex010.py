31605ff8245a33cb53875d4f313911d26883ee9a
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-08-12 07:27:15 -0300

    Learning how to use api

--- mundo1/ex010.py
+++ mundo1/ex010.py
@@ -1,7 +1,14 @@
 import tkinter as tk
 import tkinter.font as tkFont
+
+import requests
+
+
 def moedas():
     reais = float(entrada.get())
+    url = "https://economia.awesomeapi.com.br/last/USD,EUR"
+    requisicao = requests.get(url)
+    print(requisicao.json())
     dolar = reais * 0.17
     euro = reais * 0.16
     pesomexicano = reais * 3.36
@@ -11,16 +18,22 @@ def moedas():
                                f"MXN{round(pesomexicano, 3)}")
 root = tk.Tk()
 root.title('Conversor de Moedas')
-root.geometry("700x800")
+root.geometry("500x600")
 root.configure(bg="black")
-fonte = tkFont.Font(family="Helvética", size=14)
-label = tk.Label(root, text='Conversor de Moedas.', bg="black", fg="white")
+fonte = tkFont.Font(family="Helvética", size=14, weight="bold")
+label = tk.Label(root, text='Conversor de Moedas.', bg="black", fg="white", font=fonte)
 label.pack(pady=10)
 entrada = tk.Entry(root)
 entrada.pack(pady=10)
-button = tk.Button(root, text='Clique Aqui', bg="red", fg="white", command=moedas)
+button = tk.Button(root,
+                   text='Clique Aqui',
+                   bg="red", fg="white",
+                   command=moedas,
+                   font=fonte,
+                   cursor="hand2")
 button.pack(pady=10)
-button.config(width=7)
-button.config(height=5)
-label_resultado = tk.Label(root, text="", bg="black", fg="white")
+# button.config(width=7)
+# button.config(height=5)
+label_resultado = tk.Label(root, text="", bg="black", fg="white", font=fonte)
+label_resultado.pack(pady=10)
 root.mainloop()
